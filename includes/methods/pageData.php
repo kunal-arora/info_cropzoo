a0be4aadd33b8f322a156156db0765ecb6dd1b18
kunal-arora <kunal@happilyunmarried.com>
2017-06-27 00:44:56 +0530

    removed extra php include

--- includes/methods/pageData.php
+++ includes/methods/pageData.php
@@ -5,13 +5,13 @@ $_pageData = json_encode(array(
  	"crop" => array(
         "title" => "Cropzoo - Farms to Fridges",
         "description" => "xxxxxx",
-        "head_includes" => "homepage-includes-head",
+        "head_includes" => "",
         "footer_includes" => "homepage-includes-footer"
  	),
  	"zoo" => array(
         "title" => "Cropzoo - Farms to Fridges",
         "description" => "zoo",
-        "head_includes" => "homepage-includes-head",
+        "head_includes" => "",
         "footer_includes" => "homepage-includes-footer"
  	)
 ));
