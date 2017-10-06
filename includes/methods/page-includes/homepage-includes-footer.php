8cc73481d5291b147bbe4663ef4d950ddc43d401
kunal-arora <kunal_arora@live.com>
2017-10-06 16:58:26 +0530

    removed random from footer

--- includes/methods/page-includes/homepage-includes-footer.php
+++ includes/methods/page-includes/homepage-includes-footer.php
@@ -1,7 +1,6 @@
 <!-- SlidesJS Required: Link to jquery.slides.js -->
 <script src="js/vendor/slick.min.js"></script>
 <!-- End SlidesJS Required -->
-askldjakssssss
 <!-- scroll animations -->
 <!-- <script src="https://cdn.rawgit.com/michalsnik/aos/2.1.1/dist/aos.js"></script> -->
 <!-- <script type="text/javascript" src="js/vendor/wow.js"></script> -->
