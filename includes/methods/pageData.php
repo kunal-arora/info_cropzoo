4b1209238791c7b33c4b9c86e91836b561f103c2
kunal-arora <kunal_arora@live.com>
2017-11-10 18:07:43 +0530

    changes in title and description

--- includes/methods/pageData.php
+++ includes/methods/pageData.php
@@ -4,13 +4,13 @@ $_pageData = json_encode(array(
 
  	"home" => array(
         "title" => "Cropzoo - Farms to Fridges",
-        "description" => "CropZoo is a food solutions & sustainable agriculture company. We have made a commitment to produce more, produce good, conserve more and improve farmers lives.",
+        "description" => "If you have come to buy/procure fruits and vegetables for your restaurant, hotel, banquet, base kitchen or oven retail, you have come to the right place! ",
         "head_includes" => "",
         "footer_includes" => "homepage-includes-footer"
  	),
         "about" => array(
         "title" => "Cropzoo - Farms to Fridges",
-        "description" => "CropZoo is a food solutions & sustainable agriculture company. We have made a commitment to produce more, produce good, conserve more and improve farmers lives.",
+        "description" => "We started small but now we serve all. We supply fruits and vegetables to all types of businesses. Here's more about us.",
         "head_includes" => "",
         "footer_includes" => ""
         ),
