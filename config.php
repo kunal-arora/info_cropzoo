65d36526dc7b56c5a35e2dc3cdf5619cfff90372
kunal-arora <kunal_arora@live.com>
2017-12-07 16:20:37 +0530

    config changes

--- config.php
+++ config.php
@@ -5,7 +5,10 @@ $username = "root";
 $password = "root";
 $dbname = "info_cropzoo_db";
 
-
+// $servername = "localhost";
+// $username = "cropzoo_db_user";
+// $password = "WpkG0XHT(5vR";
+// $dbname = "info_cropzoo_db";
 
 
 ?>
