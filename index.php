4460482c4eba86335926abd1578770dfef908003
kunal-arora <kunal_arora@live.com>
2017-12-09 14:13:52 +0530

    home page setup

--- index.php
+++ index.php
@@ -24,12 +24,7 @@
   </head>
 
   <body id="page-top">
-    
 
-
-
-    
-    <?php  include 'includes/header.php'; ?>
 
     <!-- Intro Header -->
     <header class="masthead">
@@ -49,68 +44,78 @@
       </div>
     </header>
 
+    <?php  include 'includes/header.php'; ?>
+
     <!-- About Section -->
-    <section id="about" class="content-section text-center">
-      <div class="container">
-        <div class="row">
-          <div class="col-lg-8 mx-auto">
-            <h2>About Grayscale</h2>
-            <p>Grayscale is a free Bootstrap theme created by Start Bootstrap. It can be yours right now, simply download the template on
-              <a href="http://startbootstrap.com/template-overviews/grayscale/">the preview page</a>. The theme is open source, and you can use it for any purpose, personal or commercial.</p>
-            <p>This theme features stock photos by
-              <a href="http://gratisography.com/">Gratisography</a>
-              along with a custom Google Maps skin courtesy of
-              <a href="http://snazzymaps.com/">Snazzy Maps</a>.</p>
-            <p>Grayscale includes full HTML, CSS, and custom JavaScript files along with SASS and LESS files for easy customization!</p>
-          </div>
-        </div>
-      </div>
-    </section>
+    <div class="hompage-wrapper">
+	    <section id="about" class="content-section text-left sections">
+	      <div class="container">
+	        <div class="row">
+	          <div class="col-md-8">
+	            <h1 class="small-heading">CROPZOO</h1>
+	            <h2 class="large-heading">What do we do ?</h2>
+	            <p>We can help you eat more fruits & vegetables!</p>
+	            <p>
+	                We all know fruits and vegetables add important nutrition to our diets. But how
+	                much should we eat? And how can we make this nutrition stuff easier, tastier
+	                and more enjoyable? To answer these questions, CropZoo offers simple ways
+	                to add more fruits & veggies to your day.
+	            </p>
+	            <a href="" class="btn btn-default">CONTACT US NOW !</a>
+	          </div>
+	          <div class="col-md-4">
+				<img src="img/homepage/about_section.png" class="img-fluid right-section-image">
+	          </div>
+	        </div>
+	      </div>
+	    </section>
 
-    <!-- Download Section -->
-    <section id="download" class="download-section content-section text-center">
-      <div class="container">
-        <div class="col-lg-8 mx-auto">
-          <h2>Download Grayscale</h2>
-          <p>You can download Grayscale for free on the preview page at Start Bootstrap.</p>
-          <a href="http://startbootstrap.com/template-overviews/grayscale/" class="btn btn-default btn-lg">Visit Download Page</a>
-        </div>
-      </div>
-    </section>
+	    <!-- Download Section -->
+	    <section id="products" class="products-section content-section sections text-center">
+	      <div class="container">
+	        <div class="col-md-12">
+	          <h1 class="small-heading">PRODUCTS</h1>
+	            <h2 class="large-heading">What we offer</h2>
+	          <p>You can download Grayscale for free on the preview page at Start Bootstrap.</p>
+	          <a href="" class="btn btn-default btn-lg">CONTACT US NOW !</a>
+	        </div>
+	      </div>
+	    </section>
 
-    <!-- Contact Section -->
-    <section id="contact" class="content-section text-center">
-      <div class="container">
-        <div class="row">
-          <div class="col-lg-8 mx-auto">
-            <h2>Contact Start Bootstrap</h2>
-            <p>Feel free to leave us a comment on the
-              <a href="http://startbootstrap.com/template-overviews/grayscale/">Grayscale template overview page</a>
-              on Start Bootstrap to give some feedback about this theme!</p>
-            <ul class="list-inline banner-social-buttons">
-              <li class="list-inline-item">
-                <a href="https://twitter.com/SBootstrap" class="btn btn-default btn-lg">
-                  <i class="fa fa-twitter fa-fw"></i>
-                  <span class="network-name">Twitter</span>
-                </a>
-              </li>
-              <li class="list-inline-item">
-                <a href="https://github.com/BlackrockDigital/startbootstrap" class="btn btn-default btn-lg">
-                  <i class="fa fa-github fa-fw"></i>
-                  <span class="network-name">Github</span>
-                </a>
-              </li>
-              <li class="list-inline-item">
-                <a href="https://plus.google.com/+Startbootstrap/posts" class="btn btn-default btn-lg">
-                  <i class="fa fa-google-plus fa-fw"></i>
-                  <span class="network-name">Google+</span>
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </div>
-    </section>
+	    <!-- Contact Section -->
+	    <section id="contact" class="content-section text-center">
+	      <div class="container">
+	        <div class="row">
+	          <div class="col-lg-8 mx-auto">
+	            <h2>Contact Start Bootstrap</h2>
+	            <p>Feel free to leave us a comment on the
+	              <a href="http://startbootstrap.com/template-overviews/grayscale/">Grayscale template overview page</a>
+	              on Start Bootstrap to give some feedback about this theme!</p>
+	            <ul class="list-inline banner-social-buttons">
+	              <li class="list-inline-item">
+	                <a href="https://twitter.com/SBootstrap" class="btn btn-default btn-lg">
+	                  <i class="fa fa-twitter fa-fw"></i>
+	                  <span class="network-name">Twitter</span>
+	                </a>
+	              </li>
+	              <li class="list-inline-item">
+	                <a href="https://github.com/BlackrockDigital/startbootstrap" class="btn btn-default btn-lg">
+	                  <i class="fa fa-github fa-fw"></i>
+	                  <span class="network-name">Github</span>
+	                </a>
+	              </li>
+	              <li class="list-inline-item">
+	                <a href="https://plus.google.com/+Startbootstrap/posts" class="btn btn-default btn-lg">
+	                  <i class="fa fa-google-plus fa-fw"></i>
+	                  <span class="network-name">Google+</span>
+	                </a>
+	              </li>
+	            </ul>
+	          </div>
+	        </div>
+	      </div>
+	    </section>
+    </div>
 
     <!-- Map Section -->
     <div id="map"></div>
